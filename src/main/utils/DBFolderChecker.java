5d985af2bf23d52225521fd2fe3b13d8d2d17c3f
Malatan <40026469+Malatan@users.noreply.github.com>
2019-04-23 17:12:43 +0200

    aggiunto il checker per la cartella database

---
+++ src/main/utils/DBFolderChecker.java
@@ -0,0 +1,22 @@
+package main.utils;
+
+import java.io.File;
+
+import main.application.models.Config;
+
+public class DBFolderChecker {
+	public static boolean doCheck() {
+		SimplerSchoolUtil.confirmMsg("Cartella database corrente: " + Config.getString("config", "databaseFolder") + 
+				"\n\nAssicurati che stai usando la cartella database del tuo computer (modifica 'databaseFolder' nel config.properties)");
+		String dbPath = Config.getString("config", "databaseFolder");
+		File dbFolder = new File(dbPath);
+		if(dbFolder.exists()) {
+			Console.print("Database folder check", "fileio");
+			return true;
+		}
+		else {
+			SimplerSchoolUtil.confirmMsg("DBfolder inesistente. Configura ed esegue il file createDBFolder.bat");
+			return false;
+		}
+	}
+}
