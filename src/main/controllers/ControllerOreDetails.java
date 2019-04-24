8c1d946118ec3c04d7a27048009e68fcf6d54047
Malatan <40026469+Malatan@users.noreply.github.com>
2019-04-25 00:24:39 +0200

    aggiornamento finestra oreDetails

--- src/main/controllers/ControllerOreDetails.java
+++ src/main/controllers/ControllerOreDetails.java
@@ -5,13 +5,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import main.utils.Utils;
 import main.utils.WindowStyle;
 
 public class ControllerOreDetails {
-	
+	 @FXML
+	    private AnchorPane compitiPane;
     
 	public void initialize() {
 		initTitleBox();
