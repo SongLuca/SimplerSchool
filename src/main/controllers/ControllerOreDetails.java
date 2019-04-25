8c56e214d1cf5742b4cafcc4f77ba965c9a28f2d
Malatan <40026469+Malatan@users.noreply.github.com>
2019-04-25 04:14:35 +0200

    aggiornamento finestra dettaglio ore

--- src/main/controllers/ControllerOreDetails.java
+++ src/main/controllers/ControllerOreDetails.java
@@ -1,24 +1,104 @@
 package main.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import animatefx.animation.FadeIn;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import main.application.models.Config;
+import main.application.models.SchoolTask;
+import main.database.DataBaseHandler;
+import main.utils.Console;
 import main.utils.Utils;
 import main.utils.WindowStyle;
+import javafx.scene.control.TitledPane;
 
 public class ControllerOreDetails {
-	 @FXML
-	    private AnchorPane compitiPane;
-    
+	@FXML
+	private VBox compitiBox;
+
+	@FXML
+	private VBox verificheBox;
+
+	@FXML
+	private VBox interrBox;
+
+	private ArrayList<SchoolTask> attivita;
+	
+	private int compitiCount;
+	
+	private int verificheCount;
+	
+	private int interrCount;	
+	
 	public void initialize() {
+		this.compitiCount = 0;
+		this.verificheCount = 0;
+		this.interrCount = 0;
+		attivita = DataBaseHandler.getInstance().getAttivita();
 		initTitleBox();
+		populatePanes();
 	}
 	
+
+	public void populatePanes() {
+		if (attivita != null) {
+			for (SchoolTask task : attivita) {
+				if(task.getTipo().equalsIgnoreCase("Compito")) {
+					compitiCount++;
+					Pane content = loadTaskBox(compitiBox,compitiCount);
+					((Label)content.lookup("#idLbl")).setText(task.getIdTask()+"");
+					((Label)content.lookup("#materiaLbl")).setText("Materia: "+task.getMateria());
+					((JFXTextArea)content.lookup("#comment")).setText(task.getComment()+"");
+				}
+				
+				if(task.getTipo().equalsIgnoreCase("Verifica")) {
+					verificheCount++;
+					Pane content = loadTaskBox(verificheBox,verificheCount);
+					((Label)content.lookup("#idLbl")).setText(task.getIdTask()+"");
+					((Label)content.lookup("#materiaLbl")).setText("Materia: "+task.getMateria());
+					((JFXTextArea)content.lookup("#comment")).setText(task.getComment()+"");
+				}
+				
+				if(task.getTipo().equalsIgnoreCase("Interrogazione")) {
+					interrCount++;
+					Pane content = loadTaskBox(interrBox,interrCount);
+					((Label)content.lookup("#idLbl")).setText(task.getIdTask()+"");
+					((Label)content.lookup("#materiaLbl")).setText("Materia: "+task.getMateria());
+					((JFXTextArea)content.lookup("#comment")).setText(task.getComment()+"");
+				}
+			}
+		}
+
+	}
+
+	public Pane loadTaskBox(VBox pane, int count) {
+		try {
+			URL fxmlURL = new File(Config.getString("config", "attivitaBoxFXML")).toURI().toURL();
+			FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+			TitledPane content = fxmlLoader.load();
+			content.setText("N."+count);
+			pane.getChildren().add(content);
+			return (Pane)content.getContent().lookup("#contentPane");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 	/*********** Custom Window title bar ************/
 	@FXML
 	private HBox titleHBox;
