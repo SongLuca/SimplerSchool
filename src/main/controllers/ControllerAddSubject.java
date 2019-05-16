8d43f92df3445c4a8652927609ae5d707c529670
Malatan <40026469+Malatan@users.noreply.github.com>
2019-05-17 01:21:35 +0200

    tolto blocchi codici non piu in uso

--- src/main/controllers/ControllerAddSubject.java
+++ src/main/controllers/ControllerAddSubject.java
@@ -1,14 +1,8 @@
 package main.controllers;
-
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import com.jfoenix.controls.JFXComboBox;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import main.application.models.Docente;
 import main.application.models.Insegna;
@@ -88,85 +82,6 @@ public class ControllerAddSubject {
 			}
 		}
 
-	}
-
-	public void addVBoxToCell(GridPane osGrid, String idMateria, int row, int col, int rowSpan) {
-		Materia m = getMateriaById(idMateria);
-		VBox pane = new VBox();
-		pane.setAlignment(Pos.CENTER);
-		pane.setStyle("-fx-background-color:" + m.getColore() + ";");
-		Label lbl = new Label();
-		lbl.setText(m.getNome());
-		lbl.setId("#nomeM");
-		pane.getChildren().add(lbl);
-		pane.getStyleClass().add("calendar_pane");
-		pane.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
-			((ControllerOrarioS) MetaData.controller).addMateria(pane);
-		});
-		if (rowSpan != 1)
-			osGrid.add(pane, col, row, 1, rowSpan);
-		else
-			osGrid.add(pane, col, row);
-	}
-
-	public void fuseSubjects(GridPane osGrid, int col) {
-		LinkedHashMap<String, String> giorno = MetaData.os.getOrarioGiorno(col);
-		osGrid.getChildren().removeIf(node -> (node instanceof VBox) && GridPane.getColumnIndex(node) == col);
-		int count = 0;
-		int length = 1;
-		int startPos = 0;
-		String materiaPrec = "";
-		for (String ora : giorno.keySet()) {
-			if (count == 0) {
-				if (!giorno.get(ora).equals(""))
-					addVBoxToCell(osGrid, giorno.get(ora), startPos, col, length);
-				else {
-					VBox vPane = new VBox();
-					vPane.getStyleClass().add("calendar_pane");
-					vPane.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
-						((ControllerOrarioS) MetaData.controller).addMateria(vPane);
-					});
-					osGrid.add(vPane, col, count);
-				}
-			}
-			if (count != 0) {
-				if (!giorno.get(ora).equals("") && giorno.get(ora).equals(materiaPrec)) {
-					length++;
-					if (length == 1)
-						startPos = count;
-				} else {
-					if (length != 1) {
-						addVBoxToCell(osGrid, materiaPrec, startPos, col, length);
-						if (giorno.get(ora).equals("")) {
-							VBox vPane = new VBox();
-							vPane.getStyleClass().add("calendar_pane");
-							vPane.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
-								((ControllerOrarioS) MetaData.controller).addMateria(vPane);
-							});
-							osGrid.add(vPane, col, count);
-						} else
-							addVBoxToCell(osGrid, giorno.get(ora), count, col, 1);
-					} else {
-						length = 1;
-						startPos = count;
-						if (!giorno.get(ora).equals(""))
-							addVBoxToCell(osGrid, giorno.get(ora), startPos, col, length);
-						else {
-							VBox vPane = new VBox();
-							vPane.getStyleClass().add("calendar_pane");
-							vPane.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
-								((ControllerOrarioS) MetaData.controller).addMateria(vPane);
-							});
-							osGrid.add(vPane, col, startPos);
-						}
-					}
-					length = 1;
-					startPos = count;
-				}
-			}
-			materiaPrec = giorno.get(ora);
-			count++;
-		}
 	}
 
 	public Materia getMateriaByNome(String nome) {
